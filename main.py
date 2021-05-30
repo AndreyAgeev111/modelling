c722319f86f2850ab1a8852d2657037a59f6c781
AndreyAgeev111 <62032780+AndreyAgeev111@users.noreply.github.com>
2021-05-30 22:22:27 +0300

    +refactor

--- main.py
+++ main.py
@@ -26,18 +26,26 @@ def upgradeCurrentDensity(temperature, voltage):
                    (voltage - currentDensity(temperature, voltage) * R) / N / K / temperature) - 1)
 
 
-if __name__ == "__main__":
+def showUpgradeCurrentDensity():
+    v = np.linspace(0, 0.6, 100)
+    plt.plot(v, upgradeCurrentDensity(298, v))
+    plt.ylim(0, 8 * math.pow(10, -5))
+
+    plt.show()
+
+
+def testCurrentDensity():
     testV = np.linspace(0, 0.6, 7)
     for vol in testV:
         print('Ток прямого смещения и ток прямого смещения с поправкой:',
               " ".join([str(currentDensity(298, vol)), str(upgradeCurrentDensity(298, vol))]))
-
-    v = np.linspace(0, 0.6, 100)
-    plt.plot(v, currentDensity(298, v))
-    plt.plot(v, upgradeCurrentDensity(298, v))
+    plt.plot(testV, currentDensity(298, testV))
+    plt.plot(testV, upgradeCurrentDensity(298, testV))
     plt.ylim(0, 8 * math.pow(10, -5))
-    # plt.plot(v, upgradeCurrentDensity(323, v))
-    # plt.plot(v, upgradeCurrentDensity(373, v))
-    # plt.plot(v, upgradeCurrentDensity(423, v))
 
     plt.show()
+
+
+if __name__ == "__main__":
+    showUpgradeCurrentDensity()
+    testCurrentDensity()
