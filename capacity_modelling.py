d7ddae926ec6b85bee5f60db772d2cabcf651e06
AndreyAgeev111 <62032780+AndreyAgeev111@users.noreply.github.com>
2021-05-30 22:52:15 +0300

    +capacity modelling

---
+++ capacity_modelling.py
@@ -0,0 +1,26 @@
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+
+Q = 1.6 * math.pow(10, -19)
+E = 10 * 8.85 * math.pow(10, -12)
+ND = 7.7 * math.pow(10, 22)
+VB = 0.71
+K = 1.38 * math.pow(10, -23)
+
+
+def capacity(temperature, voltage):
+    return 2 * (VB - voltage - K * temperature / Q) / (Q * E * ND)
+
+
+def showCapacity():
+    v = np.linspace(-30, 0, 100)
+    plt.plot(v, capacity(298, v))
+    plt.xlim(-30, 0.71)
+
+    plt.show()
+
+
+def testCapacity():
+    print('Значения обратного квадрата емкости в точках  v = -30 В и V = 0.71 В:',
+          ' '.join([str(capacity(298, -30)), str(capacity(298, 0.71))]))
--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import current_modelling
+import capacity_modelling
 
 if __name__ == "__main__":
+    # ВАХ диода Шоттки
     current_modelling.showUpgradeCurrentDensity()
     current_modelling.testCurrentDensity()
+
+    # Зависимость квадрата 1/С к напряжению диода Шоттки
+    capacity_modelling.showCapacity()
+    capacity_modelling.testCapacity()
