01828ea1d647b6f355d6fb7fe3257cffadf9f96d
AndreyAgeev111 <62032780+AndreyAgeev111@users.noreply.github.com>
2021-05-30 20:29:52 +0300

    +refactor

--- main.py
+++ main.py
@@ -2,35 +2,42 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt
 
+A = 33.1
+Q = 1.6 * math.pow(10, -19)
+POTENTIAL = 0.86
+K = 1.38 * math.pow(10, -23)
+N = 1.03
+R = 159.2
+
 
 def saturationCurrentDensity(temperature):
-    return 33.1 * math.pow(temperature, 2) * np.exp(
-        -1.6 * math.pow(10, 4) * 0.86 / temperature / 1.38)
+    return A * math.pow(temperature, 2) * np.exp(
+        -Q * POTENTIAL / temperature / K)
 
 
 def currentDensity(temperature, voltage):
     return saturationCurrentDensity(temperature) * \
-           (np.exp(1.6 * math.pow(10, 4) * voltage / 1.03 / 1.38 / temperature) - 1)
+           (np.exp(Q * voltage / N / K / temperature) - 1)
 
 
 def upgradeCurrentDensity(temperature, voltage):
     return saturationCurrentDensity(temperature) * \
-           (np.exp(1.6 * math.pow(10, 4) *
-                   (voltage - currentDensity(temperature, voltage) * 159.2) / 1.03 / 1.38 / temperature) - 1)
+           (np.exp(Q *
+                   (voltage - currentDensity(temperature, voltage) * R) / N / K / temperature) - 1)
 
 
-testV = np.linspace(0, 0.6, 7)
-for vol in testV:
-    print('Ток прямого смещения и ток прямого смещения с поправкой:',
-          " ".join([str(currentDensity(298, vol)), str(upgradeCurrentDensity(298, vol))]))
+if __name__ == "__main__":
+    testV = np.linspace(0, 0.6, 7)
+    for vol in testV:
+        print('Ток прямого смещения и ток прямого смещения с поправкой:',
+              " ".join([str(currentDensity(298, vol)), str(upgradeCurrentDensity(298, vol))]))
 
-v = np.linspace(0, 0.6, 100)
-plt.plot(v, currentDensity(298, v))
-plt.plot(v, upgradeCurrentDensity(298, v))
+    v = np.linspace(0, 0.6, 100)
+    plt.plot(v, currentDensity(298, v))
+    plt.plot(v, upgradeCurrentDensity(298, v))
+    plt.ylim(0, 8 * math.pow(10, -5))
+    # plt.plot(v, upgradeCurrentDensity(323, v))
+    # plt.plot(v, upgradeCurrentDensity(373, v))
+    # plt.plot(v, upgradeCurrentDensity(423, v))
 
-
-# plt.plot(v, upgradeCurrentDensity(323, v))
-# plt.plot(v, upgradeCurrentDensity(373, v))
-# plt.plot(v, upgradeCurrentDensity(423, v))
-
-plt.show()
+    plt.show()
