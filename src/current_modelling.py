426e943ba9184d37fb5c504d8a1f4c7bfe283fe1
AndreyAgeev111 <62032780+AndreyAgeev111@users.noreply.github.com>
2021-06-16 14:17:31 +0300

    + новая аппроксимация

--- src/current_modelling.py
+++ src/current_modelling.py
@@ -9,24 +9,20 @@ POTENTIAL = 0.86
 K = 1.38 * math.pow(10, -23)
 N = 1.03
 R = 159.2
+S = 4 * math.pi * math.pow(10, -4)
 
 
-def saturationCurrentDensity(temperature):
+def saturation_current_density(temperature):
     return A * math.pow(temperature, 2) * np.exp(
         -Q * POTENTIAL / temperature / K)
 
 
-def currentDensity(temperature, voltage):
-    return saturationCurrentDensity(temperature) * \
-           (np.exp(Q * voltage / N / K / temperature) - 1)
-
-
-def upgradeSomeCurrent(temperature, voltage):
+def upgrade_some_current(temperature, voltage):
     startCurrent = 0
     currentValues = array.array('f', [])
 
     for v in range(len(voltage)):
-        current = saturationCurrentDensity(temperature) * (np.exp(
+        current = saturation_current_density(temperature) * (np.exp(
             Q * (voltage[v] - startCurrent * R) / N / K / temperature) - 1)
         currentValues.append(current)
         startCurrent = current
@@ -34,41 +30,55 @@ def upgradeSomeCurrent(temperature, voltage):
     return currentValues
 
 
-def sortedUpgradeCurrentDensity(temperature, voltage):
-    currentValues = upgradeSomeCurrent(temperature, voltage)
-    lengthOfArray = len(currentValues)
-    for current in range(lengthOfArray - 1):
-        if currentValues[current] > currentValues[(current + 1)]:
-            continue
-        else:
-            currentValues[current] = (currentValues[current - 1] + currentValues[current + 1]) / 2
+def get_oscillation_index(temperature, voltage):
+    currentValues = current_density(temperature, voltage)
+    for current in range(len(currentValues) - 1):
+        if currentValues[current] > currentValues[current + 1]:
+            return current
 
-    if currentValues[lengthOfArray - 1] < currentValues[lengthOfArray - 2]:
-        currentValues[lengthOfArray - 1] = 2 * currentValues[lengthOfArray - 2] - \
-                                           currentValues[lengthOfArray - 3]
-    currentValues[0] = 0
+
+def current_density(temperature, voltage):
+    startCurrent = 0
+    currentValues = array.array('f', [])
+
+    for v in range(len(voltage)):
+        current = saturation_current_density(temperature) * (np.exp(
+            Q * (voltage[v] - startCurrent * R * S) / N / K / temperature) - 1)
+        currentValues.append(current)
+        startCurrent = current
+
     return currentValues
 
 
-def showUpgradeCurrentDensity():
+def sort_current_density(temperature, voltage):
+    oscillationIndex = get_oscillation_index(temperature, voltage)
+    currentValues = current_density(temperature, voltage)
+    delta = currentValues[oscillationIndex] - currentValues[oscillationIndex - 1]
+
+    for current in range(oscillationIndex, len(currentValues)):
+        currentValues[current] = currentValues[current - 1] + delta
+
+    return currentValues
+
+
+def show_upgrade_current_density():
     v = np.linspace(0, 0.6, 10000)
 
-    plt.plot(v, sortedUpgradeCurrentDensity(298, v), label='T = 298 K')
-    plt.plot(v, sortedUpgradeCurrentDensity(323, v), label='T = 323 K')
-    plt.plot(v, sortedUpgradeCurrentDensity(373, v), label='T = 373 K')
-    plt.plot(v, sortedUpgradeCurrentDensity(423, v), label='T = 423 K')
+    plt.plot(v, sort_current_density(298, v), label='T = 298 K')
+    plt.plot(v, sort_current_density(323, v), label='T = 323 K')
+    plt.plot(v, sort_current_density(373, v), label='T = 373 K')
+    plt.plot(v, sort_current_density(423, v), label='T = 423 K')
 
-    plt.ylim(0, 8 * math.pow(10, 0))
+    plt.ylim(0, 3 * math.pow(10, 0))
     plt.xlabel("Напряжение, В")
     plt.ylabel("Плотность тока J, А / см^2")
     plt.legend()
     plt.show()
 
 
-def someTestValues(temperature):
+def some_test_values(temperature):
     v = np.linspace(0, 0.6, 6000)
-    currentValues = sortedUpgradeCurrentDensity(temperature, v)
-
+    currentValues = sort_current_density(temperature, v)
     # точки V = 0, 0.1, 0.2 ... 0.6
     for numb in range(6):
         print("Значение плотности тока для температуры " + str(temperature) + "К: " + str(currentValues[numb * 1000]))
--- src/main.py
+++ src/main.py
@@ -2,8 +2,8 @@ from src import capacity_modelling, current_modelling
 
 if __name__ == "__main__":
     # ВАХ диода Шоттки
-    current_modelling.showUpgradeCurrentDensity()
-    current_modelling.someTestValues(298)
+    current_modelling.show_upgrade_current_density()
+    current_modelling.some_test_values(298)
 
     # Зависимость квадрата 1/С к напряжению диода Шоттки
     capacity_modelling.showCapacity()
