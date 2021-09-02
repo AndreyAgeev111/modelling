5b1a9d7bed16660f7739f9d0586033c9d97416d8
AndreyAgeev111 <282305@niuitmo.rugit config --global user.email 282305@niuitmo.rugit config --global user.email 282305@niuitmo.ru>
2021-09-02 13:35:48 +0300

    для статьи

--- src/current_modelling_with_metal_influence.py
+++ src/current_modelling_with_metal_influence.py
@@ -8,37 +8,36 @@ Q = 1.6 * math.pow(10, -19)
 K = 1.38 * math.pow(10, -23)
 R = 159.2
 S = 4 * math.pi * math.pow(10, -4)
-T = 300
 
 
-def saturation_current_density(temperature_barrier_height):
-    return A * math.pow(T, 2) * np.exp(
-        -Q * temperature_barrier_height / T / K)
+def saturation_current_density(temperature_barrier_height, temperature):
+    return A * math.pow(temperature, 2) * np.exp(
+        -Q * temperature_barrier_height / temperature / K)
 
 
-def get_oscillation_index(temperature_barrier_height, n, voltage):
-    current_values = current_density(temperature_barrier_height, n, voltage)
+def get_oscillation_index(temperature_barrier_height, n, voltage, temperature):
+    current_values = current_density(temperature_barrier_height, n, voltage, temperature)
     for current in range(len(current_values) - 1):
         if current_values[current] > current_values[current + 1]:
             return current
 
 
-def current_density(temperature_barrier_height, n, voltage):
+def current_density(temperature_barrier_height, n, voltage, temperature):
     start_current = 0
     current_values = array.array('f', [])
 
     for v in range(len(voltage)):
-        current = saturation_current_density(temperature_barrier_height) * (np.exp(
-            Q * (voltage[v] - start_current * R * S) / n / K / T) - 1)
+        current = saturation_current_density(temperature_barrier_height, temperature) * (np.exp(
+            Q * (voltage[v] - start_current * R * S) / n / K / temperature) - 1)
         current_values.append(current)
         start_current = current
 
     return current_values
 
 
-def sort_current_density(temperature_barrier_height, n, voltage):
-    oscillation_index = get_oscillation_index(temperature_barrier_height, n, voltage)
-    current_values = current_density(temperature_barrier_height, n, voltage)
+def sort_current_density(temperature_barrier_height, n, voltage, temperature):
+    oscillation_index = get_oscillation_index(temperature_barrier_height, n, voltage, temperature)
+    current_values = current_density(temperature_barrier_height, n, voltage, temperature)
     delta = current_values[oscillation_index] - current_values[oscillation_index - 1]
 
     for current in range(oscillation_index, len(current_values)):
@@ -47,18 +46,26 @@ def sort_current_density(temperature_barrier_height, n, voltage):
 
 
 def show_upgrade_current_density():
-    v = np.linspace(-3, 2, 10000)
-
-    fig = plt.figure(figsize=(7, 4))
+    v = np.linspace(0, 3, 10000)
+    print(v[463])
+    fig = plt.figure(figsize=(7, 5))
     ax = fig.add_subplot()
-    ax.plot(v, sort_current_density(1.27, 1.05, v), label='TBH = 1.27 V, n = 1.05, Pd ', color='b')
-    ax.plot(v, sort_current_density(1.54, 1.04, v), label='TBH = 1.54 V, n = 1.04, Ni', color='k')
-    ax.plot(v, sort_current_density(1.58, 1.03, v), label='TBH = 1.58 V, n = 1.03, Pt', color='r')
-    ax.plot(v, sort_current_density(1.71, 1.09, v), label='TBH = 1.71 V, n = 1.09, Au', color='g')
+    ax.plot(v, sort_current_density(1, 1.05, v, 300), label='Mo', ls=':', lw='2')
+    ax.plot(v, sort_current_density(1.27, 1.09, v, 300), label='Au', ls='-.', lw='2')
+    ax.plot(v, sort_current_density(1.45, 1.04, v, 300), label='Pt', ls='--', lw='2')
+    ax.plot(v, sort_current_density(1.69, 1.04, v, 300), label='Ag', ls='solid', lw='2')
 
     ax.set_yscale('log')
-    ax.set_ylim([math.pow(10, -8), math.pow(10, 0)])
-    plt.xlabel("Напряжение, В")
-    plt.ylabel("Плотность тока J, А / см^2")
-    plt.legend()
+    ax.grid()
+    for label in (ax.get_xticklabels() + ax.get_yticklabels()):
+        label.set_fontsize(13)
+        label.set_fontweight('bold')
+    # ax.set_title('Al, TBH = 1.51 V, n = 1.02')
+    # ax.set_xlim([0.9, 1.2])
+    # ax.set_ylim([math.pow(10, -1), math.pow(10, 1)])
+    ax.set_ylim([math.pow(10, -8), math.pow(10, 2)])
+    ax.set_xlim([0, 3])
+    plt.xlabel("Напряжение, В", fontsize=15, fontweight='heavy', name='Arial')
+    plt.ylabel("Плотность тока J, А / см^2", fontsize=15, fontweight='heavy', name='Arial')
+    plt.legend(loc='lower right', title='T = 300 K', fontsize=15, title_fontsize=14)
     plt.show()
--- src/main.py
+++ src/main.py
@@ -3,12 +3,12 @@ from src import capacity_modelling, current_modelling, current_modelling_with_me
 
 if __name__ == "__main__":
     # ВАХ диода Шоттки
-    current_modelling.show_upgrade_current_density()
-    current_modelling.some_test_values(298)
+    # current_modelling.show_upgrade_current_density()
+    # current_modelling.some_test_values(298)
 
     # Зависимость квадрата 1/С к напряжению диода Шоттки
-    capacity_modelling.show_capacity()
-    capacity_modelling.test_capacity()
+    # capacity_modelling.show_capacity()
+    # capacity_modelling.test_capacity()
 
     # Зависимость плотности тока от материала контакта
     current_modelling_with_metal_influence.show_upgrade_current_density()
